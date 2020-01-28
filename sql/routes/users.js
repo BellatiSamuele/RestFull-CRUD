5f9acf768e25e0a71376722f4b2ea8f191e40760
BellatiS <bellati.samuele@iisgalvanimi.edu.it>
2020-01-28 12:44:55 +0000

    06.2

--- sql/routes/users.js
+++ sql/routes/users.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 const sql = require('mssql')
 const config = {
-  user: '4DD_01',  //Vostro user name
-  password: 'xxx123##', //Vostra password
+  user: 'bellati.samuele',  //Vostro user name
+  password: 'xxx123#', //Vostra password
   server: "213.140.22.237",  //Stringa di connessione
-  database: '4DD_01', //(Nome del DB)
+  database: 'bellati.samuele', //(Nome del DB)
 }
 
 /* GET users listing. */
-router.get('/attori', function(req, res, next) {
+router.get('/unit', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);  // ... error check
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from dbo.AA_ATTORE', (err, result) => {
+    sqlRequest.query('select * from dbo.[cr-unit-attributes]', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
     });
@@ -28,7 +28,7 @@ router.get('/search/:name', function(req, res, next) {
     if(err) console.log(err);
     // Query
     let sqlRequest = new sql.Request();
-    sqlRequest.query(`select * from dbo.AA_ATTORE where NOMEATTORE = '${req.params.name}'`, (err, result) => {
+    sqlRequest.query(`select * from dbo.[cr-unit-attributes] where Unit = '${req.params.name}'`, (err, result) => {
         // ... error checks
         if (err) console.log(err);
 
